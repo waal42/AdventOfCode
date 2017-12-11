83ae150d802df6d52846bae267b731873773d26d
filip.valasek <filip.valasek@oxyshop.cz>
2017-12-11 17:32:42 +0100

    day 7 part one finished

--- 2017/day07.py
+++ 2017/day07.py
@@ -1,12 +1,35 @@
 from time import time
-from pprint import pprint
+from copy import deepcopy
+# from pprint import pprint
 
 start = time()
+data = dict()
 
 with open('day07input.txt', 'r') as fin:
     data_input = fin.read().rstrip().split('\n')
-# importovat spis regularne do dictionary; klic = prvek, parametry hmotnost a list prvku, ktere nese
+    for line in data_input:
+        row = line.replace(',', '').split(' ')
+        data[row[0]] = (int(row[1].replace('(', '').replace(')', '')), [row[x] for x in range(3, len(row))])
 
+
+def part_one():
+    held = list()
+    holders = list()
+    for item in data.keys():
+        if data[item][1]:
+            holders.append(item)
+            held += data[item][1]
+    for item in holders:
+        if item not in held:
+            print(item)
+
+
+def part_two():
+    check = True
+    while check:
+        copy_of_data = deepcopy(data)
+
+
+part_two()
 runtime = time() - start
-
 print('finished in ' + str(runtime) + ' seconds')
