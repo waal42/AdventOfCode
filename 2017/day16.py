cc0bddde7f3ae991f08cf7d633be87528e3eb4bc
filip.valasek <filip.valasek@oxyshop.cz>
2017-12-20 17:35:00 +0100

    day 17

--- 2017/day16.py
+++ 2017/day16.py
@@ -2,48 +2,33 @@ from time import time
 import re
 start = time()
 
-original_dancers = 'abcdefghijklmnop'
-instructions = list()
-test_dancers = 'abcde'
-test_instructions = ['s1', 'x3/4', 'pe/b']
+with open('day16input.txt') as fin:
+    raw = fin.read()
 
 
-with open('day16input.txt', 'r') as fin:
-    instrs = fin.read().rstrip().split(',')
-    for instr in instrs:
-        tmplist = [instr[0]]
-        tmplist += re.findall(r'\w+', instr[1:])
-        instructions.append(tmplist)
-
-# print(instructions)
-
-
-def dance(instruction, dancers):
-    if instruction[0] == 's':
-        move = int(instruction[1])
-        return dancers[-move:] + dancers[:len(dancers)-move]
-    elif instruction[0] == 'x':
-        dancers_list = [x for x in dancers]
-        temp = dancers_list[int(instruction[1])]
-        dancers_list[int(instruction[1])] = dancers_list[int(instruction[2])]
-        dancers_list[int(instruction[2])] = temp
-        return ''.join(dancers_list)
-    elif instruction[0] == 'p':
-        dancers_list = [x for x in dancers]
-        indices = [dancers_list.index(instruction[1]), dancers_list.index(instruction[2])]
-        temp = dancers_list[indices[0]]
-        dancers_list[indices[0]] = dancers_list[indices[1]]
-        dancers_list[indices[1]] = temp
-        return ''.join(dancers_list)
+def solve(raw_steps):
+    steps = [d for d in raw_steps.split(',')]
+    programs = [n for n in 'abcdefghijklmnop']
+    seen = []
+    for i in range(1000000000):
+        h = tuple(programs)
+        if h in seen:
+            return ''.join(seen[1000000000 % len(seen)])
+        seen += [h]
+        for step in steps:
+            if step[0] == 's':
+                node = int(step[1:])
+                programs = programs[-node:] + programs[:-node]
+            if step[0] == 'x':
+                n1, n2 = list(map(int, step[1:].split('/')))
+                programs[n1], programs[n2] = programs[n2], programs[n1]
+            if step[0] == 'p':
+                n1, n2 = step[1:].split('/')
+                d1, d2 = programs.index(n1), programs.index(n2)
+                programs[d1], programs[d2] = n2, n1
+    return ''.join(programs)
 
 
-def solution():
-    dancers = original_dancers
-    len_instructions = len(instructions)
-    for i in range (len_instructions):
-        dancers = dance(instructions[i], dancers)
-        # print(dancers)
-    return dancers
-
+print(solve(raw))
 
 print('finished in ' + str(time() - start) + ' seconds')
