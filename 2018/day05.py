47dcfba2660182d053c3233f368157e301862c66
filip.valasek <filip.valasek@oxyshop.cz>
2018-12-07 11:21:48 +0100

    Ale fuj. Sice to funguje, ale je to hnusný a pomalý.

--- 2018/day05.py
+++ 2018/day05.py
@@ -2,57 +2,32 @@ from time import time
 
 start = time()
 
-with open("day05testinput.txt", "r") as fin:
+with open("day05input.txt", "r") as fin:
     strin = fin.read()
     polymer_list = list(strin)
     polymer_length = len(strin)
-    # print(strin)
-
-
-"""def reduce_polymer(plmr):
-    polymer = plmr
-    popped = 0
-    something_removed = False
-    for index in range(len(plmr) - 1):
-        if abs(ord(polymer[index]) - ord(polymer[index + 1]) == 32):
-            popped += 2
-            polymer = polymer[:index] + polymer[index + 2 :]
-            index -= 1
-            something_removed = True
-        if index + popped >= len(plmr) - 2:
-            break
-    if something_removed:
-        reduce_polymer(polymer)
-    else:
-        print(polymer)
-
-
-reduce_polymer(strin)"""
 
 
 def reduce_polymer(lst_plmr):
-    for index in range(len(lst_plmr) - 1):
-        if lst_plmr[index] == "":
-            continue
-        if abs(ord(lst_plmr[index]) - ord(lst_plmr[index + 1])) == 32:
-            lst_plmr[index] = ""
-            lst_plmr[index + 1] = ""
-    if "" in lst_plmr:
+    reduced = True
+    while reduced:
+        reduced = False
+        length = len(lst_plmr)
+        for index in range(0, length - 1):
+            if lst_plmr[index] == "":
+                continue
+            if abs(ord(lst_plmr[index]) - ord(lst_plmr[index + 1])) == 32:
+                lst_plmr[index] = ""
+                lst_plmr[index + 1] = ""
+                reduced = True
         lst_plmr = list(filter(None, lst_plmr))
-        reduce_polymer(lst_plmr)
-    else:
-        # print("".join(lst_plmr))
-        return len(lst_plmr)
-
-
-# reduce_polymer(polymer_list)
+    return len(lst_plmr)
 
 
 def drop_letter(lst_plmr, letter):
     return list(filter(lambda a: a != letter and a != chr(ord(letter) - 32), lst_plmr))
 
 
-# TODO - tohle zatim nefunguje
 def part_two():
     shortest = 0
     for letter in "abcdefghijklmnopqrstuvwxyz":
@@ -61,8 +36,7 @@ def part_two():
         this_length = reduce_polymer(plmr_lst)
         if letter == "a":
             shortest = this_length
-        if shortest > this_length:
-            shortest = this_length
+        shortest = min(shortest, this_length)
     print(shortest)
 
 
