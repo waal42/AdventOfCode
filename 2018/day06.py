2dad6a15d49d4546c41f66c6cdaafa9258e261f9
filip.valasek <filip.valasek@oxyshop.cz>
2018-12-11 13:34:14 +0100

    den 6 konečně hotov

--- 2018/day06.py
+++ 2018/day06.py
@@ -4,7 +4,7 @@ from pprint import pprint
 start = time()
 coordinates = dict()
 
-with open("day06testinput.txt") as fin:
+with open("day06input.txt") as fin:
     i = 1
     min_x = 1000
     min_y = 1000
@@ -24,8 +24,59 @@ with open("day06testinput.txt") as fin:
             coordinates[i]["border"] = True
         else:
             coordinates[i]["border"] = False
+        coordinates[i]["closest"] = list()
 
-# def part_one():
+
+def part_one():
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            distance = float("inf")
+            occurances = 0
+            closest = 0
+            for i in coordinates.keys():
+                this_distance = abs(x - coordinates[i]["coords"][0]) + abs(
+                    y - coordinates[i]["coords"][1]
+                )
+                if this_distance == distance:
+                    occurances += 1
+                    closest = 0
+                elif this_distance < distance:
+                    distance = this_distance
+                    occurances = 1
+                    closest = i
+            if closest in coordinates.keys():
+                coordinates[closest]["closest"].append((x, y))
+                if x == min_x or x == max_x or y == min_y or y == max_y:
+                    coordinates[closest]["border"] = True
+    print(min_x, min_y)
+    print(max_x, max_y)
+    most_closest_number = 0
+    for i in coordinates.keys():
+        if coordinates[i]["border"] == False:
+            if len(coordinates[i]["closest"]) > most_closest_number:
+                most_closest_number = len(coordinates[i]["closest"])
+    print("largest area is " + str(most_closest_number))
+
+
+def part_two():
+    # safe = list()
+    safe = 0
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            cummulative_distance = 0
+            for i in coordinates.keys():
+                cummulative_distance += abs(x - coordinates[i]["coords"][0]) + abs(
+                    y - coordinates[i]["coords"][1]
+                )
+            if cummulative_distance < 10000:
+                # safe.append((x, y))
+                safe += 1
+    # print("safe area is " + str(len(safe)))
+    print("safe area is " + str(safe))
+
+
+part_one()
+part_two()
 
 
 print("computed in " + str(time() - start) + " seconds")
