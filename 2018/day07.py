32876fa5eecb566d4e4589795eeebf8bc716d23a
filip.valasek <filip.valasek@oxyshop.cz>
2018-12-12 12:51:06 +0100

    Den 7.; celý jsem to přepsal a zesložitil kvůli chybě off by one; naštěstí už to funguje.

--- 2018/day07.py
+++ 2018/day07.py
@@ -53,53 +53,68 @@ def part_two():
     blocked = list()
     order = list()
     buffer = list()
-    workers = 5
-    working = 0
-    elapsed_time = -1
+    workers = dict()
+    workers_num = 5
+    elapsed_time = 0
     time_to_build = 60
+    for worker in range(workers_num):
+        workers[worker] = {"working": False, "working_on": ""}
     for item in sorted_parts:
         scheme[item] = {
             "needed_for": [],
             "blocked_by": [],
             "built": False,
+            "in_progress": False,
             "time_to_build": time_to_build + ord(item) - 64,
         }
     for instr in instructions:
         scheme[instr[0]]["needed_for"].append(instr[1])
         scheme[instr[1]]["blocked_by"].append(instr[0])
         blocked.append(instr[1])
-    pprint(scheme)
+    # pprint(scheme)
     while len(order) != len(sorted_parts):
-        # while elapsed_time < 20:
-        # TODO tady je to blbě udělaný, abecedně se to předřazuje, musím dělníka přímo přiřadit k součástce a ne volně
-        if workers - working != 0:
+        free_workers = [x for x in workers.keys() if workers[x]["working"] == False]
+        if len(free_workers) != 0:
             buffer = sorted(
                 [
                     x
                     for x in scheme.keys()
-                    if scheme[x]["blocked_by"] == [] and scheme[x]["built"] == False
+                    if (
+                        scheme[x]["blocked_by"] == []
+                        and scheme[x]["built"] == False
+                        and scheme[x]["in_progress"] == False
+                    )
                 ]
             )
-            working = min(workers, len(buffer))
-        for worker in range(working):
-            # print("worker " + str(worker) + " working on part " + str(buffer[worker]))
-            scheme[buffer[worker]]["time_to_build"] -= 1
+            # print("free workers: " + str(free_workers) + ", buffer: " + str(buffer))
+            to_assign = min(len(free_workers), len(buffer))
+            for worker in range(to_assign):
+                workers[free_workers[worker]]["working"] = True
+                workers[free_workers[worker]]["working_on"] = buffer[worker]
+                scheme[buffer[worker]]["in_progress"] = True
+        working_workers = [x for x in workers.keys() if workers[x]["working"] == True]
+        # print(elapsed_time, working_workers, free_workers)
+        for worker in working_workers:
             """print(
                 "worker "
                 + str(worker)
                 + " working on part "
-                + str(buffer[worker])
-                + ", time left: "
-                + str(scheme[buffer[worker]]["time_to_build"])
+                + str(workers[worker]["working_on"])
+                + " with time to built being "
+                + str(scheme[workers[worker]["working_on"]]["time_to_build"])
+                + " seconds"
             )"""
-            if scheme[buffer[worker]]["time_to_build"] == 0:
-                scheme[buffer[worker]]["built"] = True
-                order.append(buffer[worker])
-                working -= 1
-                for part in scheme[buffer[worker]]["needed_for"]:
+            scheme[workers[worker]["working_on"]]["time_to_build"] -= 1
+            if scheme[workers[worker]["working_on"]]["time_to_build"] == 0:
+                scheme[workers[worker]["working_on"]]["built"] = True
+                scheme[workers[worker]["working_on"]]["in_progress"] = False
+                order.append(workers[worker]["working_on"])
+                for part in scheme[workers[worker]["working_on"]]["needed_for"]:
                     scheme[part]["blocked_by"].pop(
-                        scheme[part]["blocked_by"].index(buffer[worker])
+                        scheme[part]["blocked_by"].index(workers[worker]["working_on"])
                     )
+                workers[worker]["working"] = False
+                workers[worker]["working_on"] = ""
         elapsed_time += 1
     print(elapsed_time, "".join(order))
 
