4e151a9d7fe1c954fffb9112a0a9c734ceea4ff3
filip.valasek <filip.valasek@oxyshop.cz>
2018-12-18 11:30:43 +0100

    osmička opsaná :(

--- 2018/day08.py
+++ 2018/day08.py
@@ -3,20 +3,36 @@ from pprint import pprint
 
 start = time()
 
-with open("day08testinput.txt") as fin:
-    tree_license = [int(x) for x in fin.read().split(" ")]
+with open("day08input.txt") as fin:
+    data = [int(x) for x in fin.read().split(" ")]
 
 
-'''def read_node(tree_list, tree_dict, parent_number):
-    count_children = tree_list[0]
-    metadata_length = tree_list[1]
-    if count_children > 0:
-        for child in range(count_children):
-            read_node(tree_list[2:])'''
+def parse(data):
+    children, metas = data[:2]
+    data = data[2:]
+    scores = list()
+    totals = 0
+    for i in range(children):
+        total, score, data = parse(data)
+        totals += total
+        scores.append(score)
+
+    totals += sum(data[:metas])
+
+    if children == 0:
+        return (totals, sum(data[:metas]), data[metas:])
+    else:
+        return (
+            totals,
+            sum(scores[k - 1] for k in data[:metas] if k > 0 and k <= len(scores)),
+            data[metas:],
+        )
 
 
+total, value, remaining = parse(data)
 
-
+print("part1: ", total)
+print("part2: ", value)
 
 
 print("computed in " + str(time() - start) + " seconds")
---
+++ 2018/day09.py
@@ -0,0 +1,9 @@
+from time import time
+from pprint import pprint
+
+start = time()
+
+
+
+
+print("computed in " + str(time() - start) + " seconds")
