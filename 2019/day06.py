b365ad0ecac76fb6d8d31a1fdca36fd9d0ff2426
filip.valasek <valasek@czc.cz>
2019-12-10 17:43:31 +0100

    když si rekurzi nakreslíš na papír, jde to

--- 2019/day06.py
+++ 2019/day06.py
@@ -5,33 +5,69 @@ start = time()
 
 direct_orbits = dict()
 
-with open("day06testinput.txt", "r") as file_in:
+with open("day06input.txt", "r") as file_in:
     for line in file_in:
         orbit = line.rstrip('\n').split(')')
         if orbit[0] in direct_orbits.keys():
             direct_orbits[orbit[0]].append(orbit[1])
         else:
             direct_orbits[orbit[0]] = [orbit[1]]
-    
-pprint(direct_orbits)
 
-# nutne revidovat rekurzi
-def find_child_orbiters(orbitee, list_of_orbiters):
+
+# pprint(direct_orbits)
+
+
+def find_child_orbiters(orbitee):
+    orbiters_list = list()
     if orbitee in direct_orbits.keys():
         for orbiter in direct_orbits[orbitee]:
-            newlist += find_child_orbiters(orbiter, list_of_orbiters)
-            list_of_orbiters.append(orbiter)
-    return list_of_orbiters
-
+            orbiters_list += [orbiter]
+            orbiters_list += find_child_orbiters(orbiter)
+        return orbiters_list
+    else:
+        return []
 
 
 def part_one():
-    print(find_child_orbiters('COM', direct_orbits['COM']))
+    count_orbits = 0
+    for planet in direct_orbits.keys():
+        child_orbits = find_child_orbiters(planet)
+        count_orbits += len(child_orbits)
+        # print(planet, child_orbits)
+    print(count_orbits)
+
 
 part_one()
 
 
+def find_node(orbitee, node, found = False):
+    if orbitee == node:
+        found = True
+        return [], found
+    path_to_node = list()
+    if orbitee in direct_orbits.keys():
+        for orbiter in direct_orbits[orbitee]:
+            path, found = find_node(orbiter, node, found)
+            if found:
+                path_to_node = [orbiter] + path + path_to_node
+                break
+        if orbitee == 'COM':
+            return path_to_node
+        return path_to_node, found
+    else:
+        return [], found
 
+
+def part_two():
+    path_to_you = find_node('COM', 'YOU')
+    path_to_santa = find_node('COM', 'SAN')
+    for index in range(len(path_to_santa)):
+        if path_to_santa[index] != path_to_you[index]:
+            break
+    print(len(path_to_you) + len(path_to_santa) - 2 * (index + 1))
+
+
+part_two()
 
 
 print("computed in " + str(time() - start) + " seconds")
