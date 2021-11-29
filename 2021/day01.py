8183913324c6e3efcd2d65d38584b7fb531ba83f
waal42 <filipvalasek@gmail.com>
2021-11-29 16:37:13 +0100

    inicializace pro advent of code 2021

---
+++ 2021/day01.py
@@ -0,0 +1,21 @@
+from time import time
+
+start = time()
+
+with open("day01input.txt", "r") as file_in:
+    pass
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+print(part_one())
+
+print(part_two())
+
+print("computed in " + str(time() - start) + " seconds")
