2ec76960575e75b8c0000c1463271222b77f6d5a
waal42 <filipvalasek@gmail.com>
2021-12-01 11:26:54 +0100

    day01

--- 2021/day01.py
+++ 2021/day01.py
@@ -3,18 +3,25 @@ from time import time
 start = time()
 
 with open("day01input.txt", "r") as file_in:
-    pass
+    depths = [int(x) for x in file_in.read().rstrip().split("\n")]
 
+# print(depths)
 
-def part_one():
-    pass
-
+def part_one(list_of_depths):
+    increases = 0
+    for i in range(len(list_of_depths) - 1):
+        if (list_of_depths[i+1] - list_of_depths[i] > 0):
+            increases += 1
+    return increases
 
 def part_two():
-    pass
+    triplets_of_depths = list()
+    for i in range(len(depths) - 2):
+        triplets_of_depths.append(depths[i] + depths[i+1] + depths[i+2])
+    return part_one(triplets_of_depths)
 
 
-print(part_one())
+print(part_one(depths))
 
 print(part_two())
 
